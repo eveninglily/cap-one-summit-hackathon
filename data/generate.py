5362d3ce27c43435c3d733b827ca563c5d5c1249
Evan McIntire <mcintire.evan@gmail.com>
2018-01-11 12:52:23 -0500

    Create function to generate purchases

--- data/generate.py
+++ data/generate.py
@@ -1,3 +1,26 @@
 from secret import *
+import requests
+import json
 
-print(NESSIE_KEY)
+def filter_merchants(category):
+    req = requests.get('http://api.reimaginebanking.com/merchants',
+                        params={'key': NESSIE_KEY})
+    for merchant in req.json():
+        for cat in merchant['category']:
+            if cat == category:
+                print(merchant['name'])
+                print(merchant['_id'])
+
+def create_purchase(id, merchant, date, amount, desc):
+    req = requests.post(
+        'http://api.reimaginebanking.com/accounts/{}/purchases?key={}'.format(id, NESSIE_KEY),
+        json={
+            "merchant_id": merchant,
+            "medium": 'balance',
+            "purchase_date": date,
+            "amount": amount,
+            "status": 'completed',
+            "description": desc
+        }
+    )
+    print(req.content)
