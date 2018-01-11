01dc877585dc0c76fb824d4ebfe835df307682d1
Haicheng Charles Zhao <czhao39@gmail.com>
2018-01-11 17:05:48 -0500

    Fix layout template and create empty login page

--- server.py
+++ server.py
@@ -6,11 +6,14 @@ from flask import *
 app = Flask(__name__, static_url_path="")
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html", isIndex=True)
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
     import sys
-
     app.run(host="0.0.0.0", port=int(sys.argv[1]) if len(sys.argv) > 1 else 8080, threaded=True)
