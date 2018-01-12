88680147ab7c83902a4b8cd76c478aef49c7a484
Haicheng Charles Zhao <czhao39@gmail.com>
2018-01-11 21:50:34 -0500

    Move data scripts to root

--- generate_purchases_data.py
+++ generate_purchases_data.py
@@ -101,4 +101,4 @@ def create_nessie_purchase(id, merchant, date, amount, desc):
     print(req.content)
 
 if __name__ == "__main__":
-    generate_nessie_purchases("5a563d1b5eaa612c093b0b1e")
+    generate_nessie_purchases(CUSTOMER_ID)
