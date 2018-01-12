76e8c9904c1e44b687d37ed41160d0c7db8818d9
Evan McIntire <mcintire.evan@gmail.com>
2018-01-11 21:46:22 -0500

    Allow creation of real nessie purchases to be genereted

--- data/generate_purchases_data.py
+++ data/generate_purchases_data.py
@@ -1,7 +1,13 @@
 import time
 import random
 import classify
+import requests
+import json
+import math
 from constants import *
+from secret import *
+
+allowed_categories = ["furniture_store", "electronics_store", "bakery", "home_goods_store", "pharmacy"]
 
 def round_to(num, dec_places):
     return round(num * 10 ** dec_places) / (10 ** dec_places)
@@ -32,6 +38,67 @@ def generate_purchases():
     purchases["other"] = [generate_purchase() for _ in range(100 - total)]
     return purchases
 
+def get_merchants_multi(categories):
+
+    return merchants
+
+# Gets a list of nessie merchants based on category, then filters by zip code
+def get_local_merchants(category, zip):
+    ret = []
+    merchants = get_merchants(category)
+    for merchant in merchants:
+        if merchant['address']['zip'] == zip:
+            ret.append(merchant)
+    return ret
+
+def get_local_merchants(categories, zip):
+    req = requests.get('http://api.reimaginebanking.com/merchants',
+                        params={'key': NESSIE_KEY})
+    merchants = []
+    print("Searching merchants")
+    for merchant in req.json():
+        for cat in merchant['category']:
+            for c2 in categories:
+                if cat == c2:
+                    if zip == None:
+                        merchants.append(merchant)
+                        break
+                    else:
+                        for z in zip:
+                            if merchant['address']['zip'] == z:
+                                merchants.append(merchant)
+                                break
+    return merchants
+
+# Creates a lot of purchases through the nessie API
+def generate_nessie_purchases(client):
+    purchase_data = generate_purchases()
+
+    merchants = {}
+    merchants["other"] = get_local_merchants(allowed_categories, None)
+    for category in GENERAL_CATEGORIES.keys():
+        print(list(GENERAL_CATEGORIES[category]))
+        merchants[category] = get_local_merchants(list(GENERAL_CATEGORIES[category]), None)
+    for category in purchase_data:
+        for purchase in purchase_data[category]:
+            i = math.floor(random.random() * len(merchants[category]))
+            id = merchants[category][i]["_id"]
+            create_nessie_purchase(client, id, list(purchase)[0], list(purchase)[1], "Purchase")
+
+
+def create_nessie_purchase(id, merchant, date, amount, desc):
+    req = requests.post(
+        'http://api.reimaginebanking.com/accounts/{}/purchases?key={}'.format(id, NESSIE_KEY),
+        json={
+            "merchant_id": merchant,
+            "medium": 'balance',
+            "purchase_date": date,
+            "amount": amount,
+            "status": 'completed',
+            "description": desc
+        }
+    )
+    print(req.content)
+
 if __name__ == "__main__":
-    p = generate_purchases()
-    classify.process(p)
+    generate_nessie_purchases("5a563d1b5eaa612c093b0b1e")
