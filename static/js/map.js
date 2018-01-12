e9fd5f0fe67339806d0302291534cfac2b64d55f
Haicheng Charles Zhao <czhao39@gmail.com>
2018-01-12 09:26:37 -0500

    Fix map zoom

--- static/js/map.js
+++ static/js/map.js
@@ -6,7 +6,7 @@ function initMap() {
     let dc = {lat: 38.9, lng: -77.01};
     map = new google.maps.Map(document.getElementById("map"), {
         center: dc,
-        zoom: 14,
+        zoom: 13,
     });
 }
 
